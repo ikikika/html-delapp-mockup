710b37971400f3def3193d375bd5f90538aa298d
ikikika <iki.jito@gmail.com>
2018-01-02 03:42:34 +0800

    complete create order page

--- create_order.php
+++ create_order.php
@@ -36,11 +36,10 @@ include("header.php");
       </div>
 
       <h4>Pick Up Details</h4>
-
-      <input type="text" class="textfield" name="date" id="datepick" placeholder="Date" data-toggle="datepicker"/>
-      <select class="select select--radius" id="select_box-2">
-        <option value="">Select Time Range</option>
-
+      <div class="content__wrapper">
+        <input type="text" class="textfield" name="date" id="datepick1" placeholder="Pick Up Date" data-toggle="datepicker"/>
+        <select class="select select--radius" id="select_box-2">
+          <option value="">Select Time Range for Pick Up</option>
           <option value="">12AM - 1AM</option>
           <option value="">1AM - 2AM</option>
           <option value="">2AM - 3AM</option>
@@ -65,125 +64,54 @@ include("header.php");
           <option value="">9PM - 10PM</option>
           <option value="">10PM - 11PM</option>
           <option value="">11PM - 12PM</option>
-
-
-      </select>
-      <input class="textfield" id="textfield-1" placeholder="Location" />
-
-      <h4>Textfield</h4>
-      <div class="content__wrapper">
-        <input class="textfield" id="textfield-1" placeholder="textfield" />
-        <input class="textfield textfield--shadow" id="textfield-2" placeholder="textfield textfield--shadow" />
-        <input class="textfield textfield--radius" id="textfield-3" placeholder="textfield textfield--radius"/>
-        <input class="textfield textfield--error" id="textfield-4" placeholder="textfield textfield--error"/>
-        <input class="textfield textfield--large" id="textfield-5" placeholder="textfield textfield--large" />
-        <span class="textfield__helper">This is helper text<span>
-        <span class="textfield__error">This is helper error message<span>
-      </div>
-
-      <h4>Text Area</h4>
-      <div class="content__wrapper">
-        <textarea class="textfield" id="textarea-1" rows="3" placeholder="textfield" ></textarea>
-        <textarea class="textfield textfield--radius" rows="3" id="textarea-3" placeholder="textfield textfield--radius"></textarea>
-      </div>
-
-      <h4>Input Select Box</h4>
-      <div class="content__wrapper">
-        <select class="select" id="select_box-1">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
         </select>
-        <select class="select select--radius" id="select_box-2">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-        <select multiple class="select" id="select_box-3">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-        <select multiple class="select select--radius" id="select_box-4">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-      </div>
+        <input class="textfield" id="autocomplete1" placeholder="Pick Up Location"
+               onFocus="geolocate()" type="text"/>
+    </div>
 
-      <h4>Checkboxes</h4>
-      <div class="content__wrapper">
-        <input class="checkbox" type="checkbox" id="checkbox-1" name"checkbox"/>
-        <label for="checkbox-1">Check 1</label>
-        <input class="checkbox" type="checkbox" id="checkbox-2" name"checkbox"/>
-        <label for="checkbox-2">Check 2</label>
-        <input class="checkbox" type="checkbox" id="checkbox-3" name"checkbox"/>
-        <label for="checkbox-3">Check 3</label>
-      </div>
+    <h4>Delivery Details</h4>
+    <div class="content__wrapper">
+    <input type="text" class="textfield" name="date" id="datepick2" placeholder="Delivery Date" data-toggle="datepicker"/>
+    <select class="select select--radius" id="select_box-2">
+      <option value="">Select Time Range for Delivery</option>
+      <option value="">12AM - 1AM</option>
+      <option value="">1AM - 2AM</option>
+      <option value="">2AM - 3AM</option>
+      <option value="">3AM - 4AM</option>
+      <option value="">4AM - 5AM</option>
+      <option value="">5AM - 6AM</option>
+      <option value="">6AM - 7AM</option>
+      <option value="">7AM - 8AM</option>
+      <option value="">8AM - 9AM</option>
+      <option value="">9AM - 10AM</option>
+      <option value="">10AM - 11AM</option>
+      <option value="">11AM - 12PM</option>
+      <option value="">12PM - 1PM</option>
+      <option value="">1PM - 2PM</option>
+      <option value="">2PM - 3PM</option>
+      <option value="">3PM - 4PM</option>
+      <option value="">4PM - 5PM</option>
+      <option value="">5PM - 6PM</option>
+      <option value="">6PM - 7PM</option>
+      <option value="">7PM - 8PM</option>
+      <option value="">8PM - 9PM</option>
+      <option value="">9PM - 10PM</option>
+      <option value="">10PM - 11PM</option>
+      <option value="">11PM - 12PM</option>
+    </select>
+    <input class="textfield" id="autocomplete2" placeholder="Delivery Location"
+        onFocus="geolocate()" type="text"/>
+    </div>
 
-      <h4>Radio</h4>
-      <div class="content__wrapper">
-        <input class="radio" type="radio" id="radio-1" name="radio"/>
-        <label for="radio-1">Yes</label>
-        <input class="radio" type="radio" id="radio-2" name="radio"/>
-        <label for="radio-2">No</label>
-      </div>
-      <div class="content__wrapper">
-        <input class="radio radio--blue" type="radio" id="radio-3" name="radio-blue"/>
-        <label for="radio-3">Yes</label>
-        <input class="radio radio--blue" type="radio" id="radio-4" name="radio-blue"/>
-        <label for="radio-4">No</label>
-      </div>
+    <h4>Receiver Information</h4>
+    <div class="content__wrapper">
+      <input class="textfield" placeholder="Receiver Name" />
+      <input class="textfield"  placeholder="Receiver contact no" />
+    </div>
 
-      <h4>Form Group</h4>
-      <div class="content__wrapper form">
-        <div class="form__group">
-          <div class="form__label">
-            I am label :
-          </div>
-          <input class="textfield" placeholder="I am placeholder" />
-        </div>
-        <div class="form__group">
-          <div class="form__label form__label--required">
-            I am required label :
-          </div>
-          <input class="textfield" placeholder="I am placeholder" />
-        </div>
-        <div class="form__group">
-          <div class="form__label">
-            I am label :
-          </div>
-          <div class="form__wrapper">
-            <input class="checkbox" type="checkbox" id="checkbox-form-1" name"checkbox-form"/>
-            <label for="checkbox-form-1">Check 1</label>
-            <input class="checkbox" type="checkbox" id="checkbox-form-2" name"checkbox-form"/>
-            <label for="checkbox-form-2">Check 2</label>
-          </div>
-        </div>
-        <div class="form__group">
-          <div class="form__label">
-            I am label :
-          </div>
-          <div class="form__wrapper">
-            <input class="radio" type="radio" id="radio-form-1" name="radio-form"/>
-            <label for="radio-form-1">Yes</label>
-            <input class="radio" type="radio" id="radio-form-2" name="radio-form"/>
-            <label for="radio-form-2">No</label>
-          </div>
-        </div>
-        <div class="form__group">
-          <div class="form__icon form__icon--radius-left">
-            Rp.
-          </div>
-          <input class="textfield" placeholder="I am placeholder" />
-        </div>
-        <div class="form__group">
-          <input class="textfield" placeholder="I am placeholder" />
-          <div class="form__icon form__icon--radius-right">
-            @gmail.com
-          </div>
-        </div>
-      </div>
+    <div class="content__wrapper">
+      <button class="button button--blue">Create Order</button>
+    </div>
 
     </article>
   <?php include("footer.php"); ?>
@@ -195,7 +123,61 @@ include("header.php");
   $('[data-toggle="datepicker"]').datepicker({
     format: 'dd-mm-YYYY'
   });
-</script>
+  // This example displays an address form, using the autocomplete feature
+      // of the Google Places API to help users fill in the information.
+
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+      var placeSearch, autocomplete;
+      var componentForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        country: 'long_name',
+        postal_code: 'short_name'
+      };
+
+      function initAutocomplete() {
+        // Create the autocomplete object, restricting the search to geographical
+        // location types.
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById('autocomplete1')),
+            {types: ['geocode']});
+
+            autocomplete = new google.maps.places.Autocomplete(
+                /** @type {!HTMLInputElement} */(document.getElementById('autocomplete2')),
+                {types: ['geocode']});
+
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+
+      }
+
+      // Bias the autocomplete object to the user's geographical location,
+      // as supplied by the browser's 'navigator.geolocation' object.
+      function geolocate() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var geolocation = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+              center: geolocation,
+              radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+          });
+        }
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDOhD5D5ZLTRab-IzodJpbRvZbbY16vhG8&libraries=places&callback=initAutocomplete"
+        async defer></script>
+    <!-- <script src="https://maps.googleapis.com/maps/api/place/autocomplete/json?input=Paris&types=geocode&key=AIzaSyCGFSybQS4yJIuGMNWQ25dcHDTYTn-bY3w"
+        async defer></script> -->
 <!-- end js -->
 
 <?php include("end.php"); ?>
