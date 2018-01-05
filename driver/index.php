50903b849dc79c4872fa6f6f896442dfdc175878
ikikika <iki.jito@gmail.com>
2018-01-05 08:10:32 +0800

    driver initial commit

---
+++ driver/index.php
@@ -0,0 +1,121 @@
+<?php
+$title = "Dashboard";
+
+include("header.php"); ?>
+
+<?php include("nav.php"); ?>
+
+<article class="main__content content">
+
+  <div class="content__wrapper">
+
+    <div class="alert alert--radius alert--orange bg-yellow">
+      <span class="alert__close">x</span>
+      <strong>Order XXXXMMDD01</strong> Driver assigned
+    </div>
+
+    <div class="alert alert--radius alert--green bg-green">
+      <span class="alert__close">x</span>
+      <strong>Order XXXXMMDD01</strong> Package delivered
+    </div>
+
+    <div class="grid__row">
+
+      <div class="grid__col-6 card__wrapper">
+        <div class="card card--orange">
+          <div class="card__header">
+            <div class="card__title">In Progress Orders</div>
+            <div class="card__tools">
+
+            </div>
+          </div>
+          <div class="card__content">
+            <div class="card__align">
+              <div class="card__big-text">
+                5
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="grid__col-6 card__wrapper">
+        <div class="card card--red">
+          <div class="card__header">
+            <div class="card__title">Completed Orders</div>
+            <div class="card__tools">
+
+            </div>
+          </div>
+          <div class="card__content">
+            <div class="card__align">
+              <div class="card__big-text">
+                12
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="grid__row">
+
+      <div class="grid__col-12 card__wrapper">
+        <div class="card card--blue">
+          <div class="card__header">
+            <div class="card__title">View Timeline</div>
+          </div>
+          <div class="card__content">
+
+            <table class="table">
+              <thead class="table__head">
+                <tr>
+                  <th>Order No.</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody class="table__body">
+                <tr>
+                  <td><a href="timeline.php">XXXXMMDD02</a></td>
+                  <td>Created <a href="" class="button button--red button-xs">Cancel</a></td>
+                </tr>
+                <tr>
+                  <td><a href="timeline.php">XXXXMMDD01</a></td>
+                  <td>Pending Pick Up</td>
+                </tr>
+                <tr>
+                  <td><a href="timeline.php">XXXXMMDD04</a></td>
+                  <td>Pending Delivery</td>
+                </tr>
+                <tr>
+                  <td><a href="timeline.php">XXXXMMDD05</a></td>
+                  <td>Completed</td>
+                </tr>
+              </tbody>
+            </table>
+
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+
+
+
+
+
+</article>
+
+<?php include("footer.php"); ?>
+
+<!-- js here -->
+
+
+<!-- end js -->
+
+<?php include("end.php"); ?>
---
+++ driver/nav.php
@@ -0,0 +1,73 @@
+<nav class="main__nav nav">
+
+  <ul class="nav__wrapper">
+    <li class="nav__item <?= $title == "Dashboard" ? "nav__item--active" : "" ?>">
+      <a href="index.php">
+        <i class="nav__icon fa fa-dashboard"></i> Dashboard
+      </a>
+    </li>
+
+    <li class="nav__item <?= $title == "Create Order" ? "nav__item--active" : "" ?>">
+      <a href="create_order.php">
+        <i class="nav__icon fa fa-certificate"></i> Create Order
+      </a>
+    </li>
+
+    <!-- <li class="nav__item ">
+      <a href="index.php">
+        <i class="nav__icon fa fa-dashboard"></i> Order History
+      </a>
+    </li> -->
+
+    <li class="nav__item <?= $title == "Edit Profile" ? "nav__item--active" : "" ?>">
+      <a href="edit_profile.php">
+        <i class="nav__icon fa fa-user"></i> Edit Profile
+      </a>
+    </li>
+
+    <li class="nav__item <?= $title == "Invoices" ? "nav__item--active" : "" ?>">
+      <a href="invoices.php">
+        <i class="nav__icon fa fa-file-text-o"></i> Invoices
+      </a>
+    </li>
+
+    <!-- <li class="nav__item nav__item--has-child">
+      <a href="#">
+        <i class="nav__icon fa fa-desktop"></i> UI Elements
+        <i class="nav__arrow fa fa-angle-down"></i>
+      </a>
+
+      <ul class="nav__wrapper nav__wrapper--child">
+        <li class="nav__item--child"><a href="typography.php">Typography</a></li>
+        <li class="nav__item--child"><a href="cards.php">Cards</a></li>
+        <li class="nav__item--child"><a href="buttons.php">Buttons</a></li>
+        <li class="nav__item--child"><a href="alert.php">Alert</a></li>
+      </ul>
+    </li>
+
+    <li class="nav__item"><a href="tables.php">
+      <i class="nav__icon fa fa-table"></i> Tables</a>
+    </li>
+    <li class="nav__item"><a href="forms.php">
+      <i class="nav__icon fa fa-edit"></i> Forms</a>
+    </li> -->
+
+  </ul>
+
+  <!-- <div class="footer__wrapper">
+
+    <div class="footer__text">
+      <span>footertext</span>
+    </div>
+
+    <ul class="footer__list">
+      <li class="footer__icon footer__icon--facebook">
+        <a rel="nofollow" href="">
+          <i class="fa fa-facebook-square"></i>
+        </a>
+      </li>
+
+    </ul>
+  </div> -->
+
+</nav>
