632739fca020e863b9b3734354cf0a243ef011e1
ikikika <iki.jito@gmail.com>
2018-01-07 15:15:51 +0800

    admin initial commit

---
+++ admin/nav.php
@@ -0,0 +1,21 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+
+
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+    <ul class="sidebar-menu" data-widget="tree">
+
+      <li class="<?= $title == "Admin Dashboard" ? "active" : "" ?>">
+        <a href="index.php">
+          <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+        </a>
+      </li>
+
+      
+
+    </ul>
+  </section>
+  <!-- /.sidebar -->
+</aside>
